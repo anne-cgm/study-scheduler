e9d8747c9ba6efc784605ff71bab70a1abf7f04d
anne-cgm <anniie@sempreceub.com>
2026-04-07 16:06:07 -0300

    chore: update project version to 1.0.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,11 @@
-module com.acgm.studyscheduler {
+module studyscheduler {
 
     requires javafx.controls;
     requires javafx.fxml;
 
-    // Permitir que o FXML acesse o controller
     opens studyscheduler.controller to javafx.fxml;
 
-    // Se o FXML estiver em package "view" mesmo:
     opens view to javafx.fxml;
 
-    // Exporta o pacote onde está o Main
     exports studyscheduler.app;
 }
